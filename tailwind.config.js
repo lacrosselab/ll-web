5a91eaa215d0123628f5e2785ba4378befa68337
Pete Wallace <pete@Mac.lan>
2025-10-07 13:29:29 -0400

    about page stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        'futura': ['Futura', 'sans-serif'],
+        'futura-bold': ['Futura', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
